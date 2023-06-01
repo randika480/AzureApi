014e81f381c0b0cfdbb93ecfdee136b12f07e706
Thusitha Disanayake <thusitha.dissanayake@seer365.com>
2023-06-01 10:17:19 +0530

    Bug Fixes

---
+++ SeerAzureWebAPI/Controllers/TaskController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.VisualStudio.Services.WebApi.Patch;
+using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using System;
+
+namespace SeerAzureWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/workitems")]
+    public class TaskController : ControllerBase
+    {
+        private readonly string _uri;
+        private readonly string _personalAccessToken;
+        private readonly string _project;
+        private readonly IConfiguration _configuration;
+
+        public TaskController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _uri = configuration.GetSection("AzureDevOpsSettings:Uri").Value;
+            _personalAccessToken = configuration.GetSection("AzureDevOpsSettings:PersonalAccessToken").Value;
+            _project = configuration.GetSection("AzureDevOpsSettings:Project").Value;
+        }
+
+        [HttpGet]
+        public IActionResult GetWorkItems()
+        {
+            try
+            {
+                var workItem = CreateTaskUsingClientLib();
+
+                if (workItem != null)
+                {
+                    return Ok(workItem);
+                }
+                else
+                {
+                    return BadRequest("Failed to create task");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        private WorkItem CreateTaskUsingClientLib()
+        {
+            Uri uri = new Uri(_uri);
+            string personalAccessToken = _personalAccessToken;
+            string project = _project;
+
+            VssBasicCredential credentials = new VssBasicCredential("", _personalAccessToken);
+            JsonPatchDocument patchDocument = new JsonPatchDocument();
+
+            // Add fields and their values to your patch document
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/fields/System.Title",
+                    Value = "Task Created Using API"
+                }
+            );
+
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/fields/Microsoft.VSTS.TCM.ReproSteps",
+                    Value = "Our authorization logic needs to allow for users with Microsoft accounts (formerly Live Ids) - http:// msdn.microsoft.com/library/live/hh826547.aspx"
+                }
+            );
+
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/fields/Microsoft.VSTS.Common.Priority",
+                    Value = "1"
+                }
+            );
+
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/fields/Microsoft.VSTS.Common.Severity",
+                    Value = "2 - High"
+                }
+            );
+
+            VssConnection connection = new VssConnection(uri, credentials);
+            WorkItemTrackingHttpClient workItemTrackingHttpClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            try
+            {
+                WorkItem result = workItemTrackingHttpClient.CreateWorkItemAsync(patchDocument, project, "Task").Result;
+                Console.WriteLine("Task Successfully Created: Task #{0}", result.Id);
+                return result;
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("Error creating Task: {0}", ex.InnerException.Message);
+                return null;
+            }
+        }
+    }
+}
+
---
+++ SeerAzureWebAPI/Startup/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.TeamFoundation.Build.WebApi;
+
+namespace SeerAzureWebAPI.Startup
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container
+            services.AddControllers();
+
+            //modify the ConfigureServices method to inject the IConfiguration instance and make it available for dependency injection
+            services.AddSingleton<IConfiguration>(Configuration);
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
